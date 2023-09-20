b91dd2046bcdf6fae9ca186a49bc4eb28370141c
PIEJIN <koroliov.radislav@gmail.com>
2023-09-21 01:55:43 +0800

    9try

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -4,9 +4,9 @@ from decouple import config
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
 
-DEBUG = config('DEBUG')
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
