61ca8c6d1669fb2e8ac1fea107a1368dcfffa8e1
PIEJIN <koroliov.radislav@gmail.com>
2023-09-21 02:29:50 +0800

    11try

--- backend/kittygram_backend/urls.py
+++ backend/kittygram_backend/urls.py
@@ -12,7 +12,7 @@ router.register(r'achievements', AchievementViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-    path('api/', include('djoser.urls')),  # Работа с пользователями
+    path('api/', include('djoser.urls')),  # Работа с пользователями.
     path('api/', include('djoser.urls.authtoken')),  # Работа с токенами
 ]
 
