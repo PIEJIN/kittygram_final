874494bc233f8271856f0dfb45c4a16bbf80eb5b
PIEJIN <koroliov.radislav@gmail.com>
2023-09-27 02:04:57 +0800

    fix SECRET_KEY

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -2,7 +2,9 @@ import os
 from pathlib import Path
 
 from decouple import config
+from dotenv import load_dotenv
 
+load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', default='token')
