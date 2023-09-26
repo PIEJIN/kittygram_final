2f73b6696f8810747c37dd2755d5b7934805fbc2
PIEJIN <koroliov.radislav@gmail.com>
2023-09-27 01:23:16 +0800

    fix SECRET_KEY

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,14 +1,16 @@
 from pathlib import Path
 
-from decouple import config
+from decouple import Config
+
+config = Config('.env')
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+secret_key = config.get('SECRET_KEY', default='default_secret_key')
 
-DEBUG = config('DEBUG_MODE', 'False').lower() == 'true'
+DEBUG = config.get('DEBUG_MODE', default='False').lower() == 'true'
 
-ALLOWED_HOSTS = config('ALLOWED_HOST', default='127.0.0.1, localhost').split(', ')
+ALLOWED_HOSTS = config.get('ALLOWED_HOST', default='127.0.0.1, localhost').split(', ')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
