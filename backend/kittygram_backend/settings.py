8bcc0b33f93068cd0bfc186deda86bae0e4df08e
PIEJIN <koroliov.radislav@gmail.com>
2023-09-27 02:15:35 +0800

    fix SECRET_KEY

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,13 +1,10 @@
-import os
 from pathlib import Path
 
 from decouple import config
-from dotenv import load_dotenv
 
-load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY', default='token')
+SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
 
 DEBUG = config('DEBUG_MODE', 'False').lower() == 'true'
 
